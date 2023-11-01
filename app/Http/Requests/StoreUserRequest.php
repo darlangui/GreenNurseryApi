72a00b9acfdb4e9ad7a680e54aea51998189e73b
Darlan Guimarães <71740181+darlangui@users.noreply.github.com>
2023-11-01 19:43:26 -0300

    fix: error name create user

--- app/Http/Requests/StoreUserRequest.php
+++ app/Http/Requests/StoreUserRequest.php
@@ -23,7 +23,7 @@ class StoreUserRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => 'required', 'string', 'min:1', 'max:100',
+            'name' => ['required', 'string', 'min:1', 'max:100'],
             'email' => ['required', 'string', 'min:5', 'max:200', Rule::unique('users')],
             'password' => ['required', 'string', 'min:8'],
         ];
